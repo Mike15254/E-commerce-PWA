ae6461a5edc4d075ec9d8613ca5c7827df3ad9c5
Mike15254 <mike8max10@gmail.com>
2023-08-01 02:38:52 +0300

    update

--- Ecommerce-pwa/src/routes/+page.svelte
+++ Ecommerce-pwa/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let name="Mike";
+</script>
+
+<h1>Hello world {name}</h1>
