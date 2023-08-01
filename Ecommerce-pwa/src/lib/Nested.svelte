7183ca5d1313269802b164d62c47d84ba24f3373
Mike15254 <mike8max10@gmail.com>
2023-08-02 01:05:42 +0300

    updated

---
+++ Ecommerce-pwa/src/lib/Nested.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+   
+    // keyed each blocks
+    const emojis = {
+		apple: '🍎',
+		banana: '🍌',
+		carrot: '🥕',
+		doughnut: '🍩',
+		egg: '🥚'
+	};
+
+    export let name1:keyof typeof emojis;
+
+    const emoji = emojis[name1];
+</script>
+
+
+<p>{emoji} = {name1}</p>
