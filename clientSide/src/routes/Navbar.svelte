e91a3834ccaddf36959c5d36dd3b239c8918a56c
Mike15254 <mike8max10@gmail.com>
2023-09-13 21:49:21 +0300

    Update: update on the css

--- clientSide/src/routes/Navbar.svelte
+++ clientSide/src/routes/Navbar.svelte
@@ -7,6 +7,7 @@
 		<div class="logo">
 			<a href="/">
 				<img src={logo} alt="Mzito Supermarket" />
+			
 			</a>
 		</div>
 		<div class="search-product">
@@ -24,17 +25,17 @@
 				>
 			</button>
 		</div>
-		<div class="About">
+		<div class="about">
 			<a href="/"> About </a>
 		</div>
 		<div class="cart-btn">
 			<a href="/" class="cart">
-				<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"
+				<svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512"
 					><path
 						d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"
 					/></svg
 				>
-				Cart
+				<span>Cart</span>
 			</a>
 			<span class="cart-notification">
 				<span class="visually-hidden">1</span>
@@ -42,31 +43,39 @@
 		</div>
 		<div class="user-btn">
 			<a href="/" class="user">
-				<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"
+				<svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512"
 					><path
 						d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
 					/></svg
 				>
-				Account
+				<span>Account</span>
 			</a>
 		</div>
 	</header>
 </div>
 
 <style>
-	/* .header-sty {
-        position: absolute;
-        left: 100px;
-        width: calc(100% - 200px);
-    } */
+	.header-sty {
+        position: relative;
+        display: flex;
+		margin-left: 10em;
+		margin-right: 10em;
+    }
 	header {
-		position: absolute;
+		flex: 1;
 		display: flex;
 		justify-content: space-around;
 		align-items: center;
-		left: 150px;
-		width: calc(100% - 300px);
 		padding-top: 20px;
+	}
+	a {
+		text-decoration: none;
+		color: #ececec;
+	}
+	.about {
+		display: flex;
+		text-align: center;
+		padding: 10px;
 	}
 
 	.logo {
@@ -87,6 +96,7 @@
 		height: 8em;
 		object-fit: contain;
 	}
+	
 	.search-product {
 		display: flex;
 		align-items: center;
@@ -95,7 +105,7 @@
 		background: rgba(17, 17, 17, 0.8);
 		backdrop-filter: blur(4px);
 		-webkit-backdrop-filter: blur(4px);
-		border-radius: 10px;
+		border-radius: 0.625em;
 		border: 1px solid rgba(255, 255, 255, 0.18);
 	}
 	.form-control {
@@ -127,4 +137,10 @@
 	svg {
 		fill: #daef68;
 	}
+	.user-btn span, .cart-btn span {
+		text-align: center;
+		padding: 3px;
+	}
+
+	
 </style>
