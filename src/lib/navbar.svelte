56fe956d6fbe7e14f9a9669b4cbf57004c7a3ee0
Mike15254 <mike8max10@gmail.com>
2023-10-16 23:54:32 +0300

    Update: refactored the code base

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,7 @@
+<script>
+    import img1 from "../assets/zito.png"
+</script>
+
+<div>
+    <img src={img1} alt="">
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Navbar from "$lib/navbar.svelte";
+    import "$lib/styles.css"
+</script>
+
+<div class="app">
+	<Navbar/>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>@ 2023 Mzito. All rights reserved</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+    		display: flex;
+		flex-direction: column;
+	}
+</style>
