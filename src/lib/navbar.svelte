a5e207767dc17d77cea1cfab3593ab4aeace9843
Mike15254 <mike8max10@gmail.com>
2023-10-23 04:15:09 +0300

    Update: refactored the code base

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,7 +1,37 @@
 <script>
-    import img1 from "../assets/zito.png"
+	import logo from '../assets/zito.png';
+	let locations = ['Bomas', 'Embassy', 'Town'];
+	let selectedLocation = 'Bomas'; // Default location
+
+	function selectLocation(event) {
+		selectedLocation = event.target.value;
+	}
 </script>
 
 <div>
-    <img src={img1} alt="">
+	<img src={logo} alt="" />
 </div>
+
+<div class="delivery">
+	<button class="delivery-button">
+		<i class="location-icon">📍</i>
+		<!-- Replace with your icon -->
+		Deliver to
+		<select bind:value={selectedLocation} on:change={selectLocation}>
+			{#each locations as location}
+				<option value={location}>{location}</option>
+			{/each}
+		</select>
+	</button>
+</div>
+
+<style>
+    .delivery-button {
+      display: flex;
+      align-items: center;
+    }
+  
+    .location-icon {
+      margin-right: 0.5rem;
+    }
+  </style>
